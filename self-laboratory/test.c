3ed576a6f075479cf1ac0d61e959ff49288635a8
werifu <wohu1363195380@outlook.com>
2020-12-09 23:03:32 +0800

    add machine level thought

--- self-laboratory/test.c
+++ self-laboratory/test.c
@@ -1,7 +1,27 @@
 #include<stdio.h>
 
+int switch_func(int x) {
+	int result = 15;
+	switch (x) {
+	case 1:
+		result++;
+		break;
+	case 2:
+		result = 2;
+		break;
+	case 3:
+		result++;
+	case 4: 
+		result = 4;
+		break;
+	default:
+		result = 10;
+		break;
+	}
+	return result;
+}
 int main() {
-	int x = 0x80000000;
-	printf("%d",!!x);
+	int i = switch_func(233);
+
 	return 0;
 }
--- self-laboratory/test.s
+++ self-laboratory/test.s
@@ -1,34 +1,44 @@
 	.file	"test.c"
 	.text
-	.section	.rodata.str1.1,"aMS",@progbits,1
-.LC0:
-	.string	"%d\n"
-	.text
-	.globl	main
-	.type	main, @function
-main:
+	.globl	switch_func
+	.type	switch_func, @function
+switch_func:
 .LFB11:
 	.cfi_startproc
-	pushq	%rbx
-	.cfi_def_cfa_offset 16
-	.cfi_offset 3, -16
-	movl	$0, %ebx
-	jmp	.L2
+	cmpl	$2, %edi
+	je	.L4
+	jg	.L3
+	cmpl	$1, %edi
+	jne	.L5
+	movl	$16, %eax
+	ret
 .L3:
-	leal	(%rbx,%rbx,4), %esi
-	leaq	.LC0(%rip), %rdi
-	movl	$0, %eax
-	call	printf@PLT
-	addl	$1, %ebx
-.L2:
-	cmpl	$4, %ebx
-	jle	.L3
-	movl	$0, %eax
-	popq	%rbx
-	.cfi_def_cfa_offset 8
+	subl	$3, %edi
+	cmpl	$1, %edi
+	ja	.L7
+	movl	$4, %eax
+	ret
+.L7:
+	movl	$10, %eax
+	ret
+.L4:
+	movl	%edi, %eax
+	ret
+.L5:
+	movl	$10, %eax
 	ret
 	.cfi_endproc
 .LFE11:
+	.size	switch_func, .-switch_func
+	.globl	main
+	.type	main, @function
+main:
+.LFB12:
+	.cfi_startproc
+	movl	$0, %eax
+	ret
+	.cfi_endproc
+.LFE12:
 	.size	main, .-main
 	.ident	"GCC: (GNU) 10.2.0"
 	.section	.note.GNU-stack,"",@progbits
